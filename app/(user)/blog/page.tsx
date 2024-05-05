31144682f28afbf29b5ae02662a322975f9c2c5a
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-05 15:29:58 +0300

    add

--- app/(user)/blog/page.tsx
+++ app/(user)/blog/page.tsx
@@ -1,8 +1,8 @@
-import { groq } from "next-sanity";
+ import { groq } from "next-sanity";
 import { client } from "../../../lib/sanity.client";
 import MainBlogs from "./mainBlogs";
 
-// export const revalidate = 30;
+ export const revalidate = 30;
 
 const query = groq`
   *[_type=='post'] {
@@ -31,12 +31,7 @@ groq`*[_type == "category"] {
   "count": count(*[_type == "post" && references(^._id)])
 } | order(count desc) [0...5]`;
 
-// groq`
-//   *[_type=='category'] {
-//     ...,
-    
-//   } | order(_createdAt desc)
-// `;
+
 
 
 
@@ -57,7 +52,6 @@ export const metadata = {
     icon: "/assets/img/sm-logo.svg",
   },
 };
-
 interface Props { 
   searchParams:{
    
@@ -66,10 +60,7 @@ interface Props {
   }
 }
 
-
-
-
-export default async function BlogsPagess({searchParams} : Props) {
+const Blogs = async ({searchParams}:Props) => {
   const { category ,tag} = searchParams
 
 
@@ -88,17 +79,113 @@ export default async function BlogsPagess({searchParams} : Props) {
 
 
   console.log(catsData)
-//?category=development
-
-
-
 
   return (
     <>
-      <MainBlogs newpostsData={newpostsData} tagsData={tagsData} catsData={catsData} blogs={blogs} />
+  <MainBlogs newpostsData={newpostsData} tagsData={tagsData} catsData={catsData} blogs={blogs}/>
     </>
   );
 };
 
-// export default HomePage;
-// export const dynamic = 'force-dynamic'
+export default Blogs;
+
+
+
+
+// import { groq } from "next-sanity";
+// import { client } from "../../../lib/sanity.client";
+// import MainBlogs from "./mainBlogs";
+
+//  export const revalidate = 30;
+
+// const query = groq`
+//   *[_type=='post'] {
+//     ...,
+//     author->,
+//     categories[]->
+//   } | order(_createdAt desc)
+// `;
+
+
+
+// const nepostsquery = groq`
+//   *[_type=='post'] {
+//     ...,
+//     author->,
+//     categories[]-> 
+//   } | order(_createdAt desc) [0...3]
+// `;
+
+
+
+
+// const catsquery = 
+// groq`*[_type == "category"] {
+//   ...,
+//   "count": count(*[_type == "post" && references(^._id)])
+// } | order(count desc) [0...5]`;
+
+
+
+
+
+// const tagsquery = 
+
+// groq`
+//   *[_type=='tag'] {
+//     ...,
+ 
+//   } | order(_createdAt desc)
+// `;
+
+
+
+
+// export const metadata = {
+//   icons: {
+//     icon: "/assets/img/sm-logo.svg",
+//   },
+// };
+
+// interface Props { 
+//   searchParams:{
+   
+//     category?:string
+//     tag?: any
+//   }
+// }
+
+
+
+
+// export default async function BlogsPagess({searchParams} : Props) {
+//   const { category ,tag} = searchParams
+
+
+//   const productFilter = `_type == "post"`
+//   const categoryFilter = category ? `&& "${category}" in categories[]->title ` :""
+//   const tagFilter = tag ? `&& "${tag}" in tags[]->title ` :""
+//   const filter = `*[${productFilter}${categoryFilter}${tagFilter}]`
+
+//   const blogs = await client.fetch(groq`${filter} {
+//   ...
+//   }| order(_createdAt desc)`); ;
+
+//   const newpostsData = await client.fetch(nepostsquery);
+//   const catsData = await client.fetch(catsquery);
+//   const tagsData = await client.fetch(tagsquery);
+
+
+//   console.log(catsData)
+// //?category=development
+
+
+
+
+//   return (
+//     <>
+//       <MainBlogs newpostsData={newpostsData} tagsData={tagsData} catsData={catsData} blogs={blogs} />
+//     </>
+//   );
+// };
+
