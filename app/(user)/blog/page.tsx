907b99a735454acb5edfa93f7a76fae0f4beb881
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-05 15:45:57 +0300

    add

--- app/(user)/blog/page.tsx
+++ app/(user)/blog/page.tsx
@@ -2,7 +2,7 @@
 import { client } from "../../../lib/sanity.client";
 import MainBlogs from "./mainBlogs";
 
-//  export const revalidate = 30;
+  export const revalidate = 5;
 
 const query = groq`
   *[_type=='post'] {
